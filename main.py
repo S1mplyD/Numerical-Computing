b4e28a587155800a7a9b742196b46582cadad1bd
S1mplyD <lucabennati2k16@gmail.com>
2022-01-10 08:54:36 +0100

    Bug Fix and Log
    Fixed norm bug, and added logs to print on file

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from scipy import signal
 from scipy.optimize import minimize
 from numpy import fft
 
+
 # Create a Gaussian kernel of size kernlen and standard deviation sigma
 def gaussian_kernel(kernlen, sigma):
     x = np.linspace(- (kernlen // 2), kernlen // 2, kernlen)    
@@ -43,86 +44,96 @@ def AT(x, K):
 #Punto 1: Blur dell'immagine
 
 #Import dell'immagine
-image = io.imread('/content/imagecalc8.png')  
+image = io.imread('./imagecalc7.png')  
 
 #Definisco il tipo dell'immagine e normalizzo l'immagine
-X = image.astype(np.float64) / 255.0
-m, n = X.shape
+Xoriginal = image.astype(np.float64) / 255.0
+m, n = Xoriginal.shape
 
 # Genera i diversi filtri di blur
-K = psf_fft(gaussian_kernel(5, 0.5), 5, X.shape)
-K1 = psf_fft(gaussian_kernel(7, 1), 7, X.shape)
-K2 = psf_fft(gaussian_kernel(9, 1.3), 9, X.shape)
+#K= psf_fft(gaussian_kernel(5, 0.5), 5, Xoriginal.shape)
+#K = psf_fft(gaussian_kernel(7, 1), 7, Xoriginal.shape)
+K = psf_fft(gaussian_kernel(9, 1.3), 9, Xoriginal.shape)
 
 # Genera il rumore
-dev = 0.033660826503758685 # np.random.uniform(low=0.0, high=0.05)
-noise = np.random.normal(size=X.shape) * dev
+dev = 0.01218556955205374#np.random.uniform(low=0.0, high=0.05)
+noise = np.random.normal(size=Xoriginal.shape) * dev
+
+print(dev)
 
 # Aggiungi i vari livelli di blur e il rumore, per generare diverse immagini corrotte
-b = A(X, K) + noise
-b1 = A(X, K1) + noise
-b2 = A(X, K2) + noise
+b = A(Xoriginal, K) + noise
 
-#Calcolo del PSNR
-PSNR = metrics.peak_signal_noise_ratio(X, b)
-PSNR1 = metrics.peak_signal_noise_ratio(X, b1)
-PSNR2 = metrics.peak_signal_noise_ratio(X, b2)
+ATb = AT(b,K)
 
-#Calcolo MSE
-MSE = metrics.mean_squared_error(X, b)
-MSE1 = metrics.mean_squared_error(X, b1)
-MSE2 = metrics.mean_squared_error(X, b2)
+#Calcolo del PSNR tra immagine originale e corrotta
+PSNR = metrics.peak_signal_noise_ratio(Xoriginal, b)
+
+#Calcolo MSE tra immagine reale e corrotta
+MSE = metrics.mean_squared_error(Xoriginal, b)
 
 #Punto 2: Soluzione naive
+
+file = open("results.txt", "a")
 
 #Funzione che è necessario minimizzare per ottenere il risultato con soluzione naive
 def f(x):
   J = x.reshape(m,n)
-  res = 0.5*(np.linalg.norm(A(J, K)-b))**2
-  return np.sum(res)
+  res = 0.5*(np.linalg.norm(A(J, K)-b,ord=2))**2
+  file.write("funzione obiettivo f: ")
+  file.write(str(res))
+  file.write("\n")
+  return res
 
 #Funzione per il calcolo del gradiente
 def df(x):
   J = x.reshape(m,n)
-  res = AT(A(J, K)-b, K)
+  res = AT(A(J, K), K) - ATb
+  # print(f"norma gradiente df: {res}")
+  file.write("norma gradiente df: ")
+  file.write(str(res))
+  file.write("\n")
   RES = np.reshape(res, m*n)
   return RES
 
-result = minimize(f,X,method='CG',jac=df)
 
-"""
-Dal risultato della funzione minimize pongo in X1 un array contenente la matrice minimizzata,
-ne eseguo il reshape per dargli la dimensione (512,512).
-Normalizzo poi l'immagine
-"""
-Xnaive = np.array([result.x])
+
+x0 = np.zeros(m*n)
+#Immagine ricostruita con metodo naive Kernel 5x5
+naive = minimize(f,x0,method='CG',jac=df)
+file.write("iterazioni naive: ")
+file.write(str(naive.nit))
+file.write("\n")
+
+#immagini ricostruite
+Xnaive = np.array([naive.x])
+
 Xnaive = Xnaive.reshape(m,n)
-X2naive = Xnaive.astype(np.float64) / 255
 
-#Calcolo PSNR dell'immagine a cui è stato applicato il deblur con soluzione naive
-b3 = A(X2naive,K) + noise
-PSNR3 = metrics.peak_signal_noise_ratio(X2naive,b3)
-MSE3 = metrics.mean_squared_error(X2naive, b3)
-b31 = A(X2naive,K1) + noise
-PSNR31 = metrics.peak_signal_noise_ratio(X2naive,b31)
-MSE31 = metrics.mean_squared_error(X2naive, b31)
-b32 = A(X2naive,K2) + noise
-PSNR32 = metrics.peak_signal_noise_ratio(X2naive,b32)
-MSE32 = metrics.mean_squared_error(X2naive, b32)
+#Calcolo PSNR e MSE tra immagine originale e ricostruita
+PSNRnaive = metrics.peak_signal_noise_ratio(Xoriginal,Xnaive)
+MSEnaive = metrics.mean_squared_error(Xoriginal, Xnaive)
 
 #Punto 3: Regolarizzazione
 
 #Valore di lambda selezionato
-_lambda = 0.5
+_lambda = 0.04
 
 def freg(x):
   J = x.reshape(m,n)
   res = 0.5*(np.linalg.norm(A(J, K)-b))**2 + (_lambda/2)*(np.linalg.norm(J))**2
-  return np.sum(res)
+  file.write("funzione obiettivo freg: ")
+  file.write(str(res))
+  file.write("\n")
+  return res
 
 def dfreg(x):
   J = x.reshape(m,n)
-  res = AT(A(J, K)-b, K) + _lambda*J
+  res = AT(A(J, K), K) - ATb + _lambda*J
+  # print(f"norma gradiente dfreg: {res}")
+  file.write("norma gradiente dfreg: ")
+  file.write(str(res))
+  file.write("\n")
   RES = np.reshape(res, m*n)
   return RES
 
@@ -153,120 +164,68 @@ def minimize2(x0): # funzione che implementa il metodo del gradiente
     x_last=x_last-step*grad.reshape(m,n) 
   return (x_last, k)
 
-#risultato minimize scipy.optimize
-result2 = minimize(freg,Xnaive,method='CG',jac=dfreg)
-#risultato minimize implementata a lezione
-result3 = minimize2(Xnaive)
+#risultato minimize gradiente coniugato
+regCG = minimize(freg,x0,method='CG',jac=dfreg)
+file.write("iterazioni regCG: ")
+file.write(str(regCG.nit))
+file.write("\n")
+#risultato minimize gradiente implementato a lezione
+regG = minimize2(x0)
+file.write("iterazioni regG: ")
+file.write(str(regG[1]))
+file.write("\n")
+print(f"iterazioni regG: {regG[1]}")
+file.close()
+#immagini ricostruite
+XregCG = np.array([regCG.x])
 
-Xreg = np.array([result2.x])
-Xreg = Xreg.reshape(m,n)
-X2reg = Xreg.astype(np.float64) / 255
+XregCG = XregCG.reshape(m,n)
 
-b4 = A(X2reg,K) + noise
-PSNR4 = metrics.peak_signal_noise_ratio(X2reg,b4)
-MSE4 = metrics.mean_squared_error(X2reg, b4)
 
-b41 = A(X2reg,K1) + noise
-PSNR41 = metrics.peak_signal_noise_ratio(X2reg,b41)
-MSE41 = metrics.mean_squared_error(X2reg, b41)
+XregG = np.array(regG[0])
 
-b42 = A(X2reg,K2) + noise
-PSNR42 = metrics.peak_signal_noise_ratio(X2reg,b42)
-MSE42 = metrics.mean_squared_error(X2reg, b42)
+XregG = XregG.reshape(m,n)
 
-Xreg2 = np.array(result3[0])
-Xreg2 = Xreg2.reshape(m,n)
-X2reg2 = Xreg2.asype(np.float64) / 255
+#Calcolo PSNR e MSE tra immagine originale e immagine ricreata tramite metodo gradienti coniugati
+PSNRregCG = metrics.peak_signal_noise_ratio(Xoriginal,XregCG)
+MSEregCG = metrics.mean_squared_error(Xoriginal, XregCG)
 
-b5 = A(X2reg2,K) + noise
-PSNR5 = metrics.peak_signal_noise_ratio(X2reg2,b5)
-MSE5 = metrics.mean_squared_error(X2reg2, b5)
-
-b51 = A(X2reg2,K1) + noise
-PSNR51 = metrics.peak_signal_noise_ratio(X2reg2,b51)
-MSE51 = metrics.mean_squared_error(X2reg2, b51)
-
-b52 = A(X2reg2,K2) + noise
-PSNR52 = metrics.peak_signal_noise_ratio(X2reg2,b52)
-MSE52 = metrics.mean_squared_error(X2reg2, b52)
+#Calcolo PSNR e MSE tra immagine originale e immagine ricreata tramite metodo del gradiente
+PSNRregG = metrics.peak_signal_noise_ratio(Xoriginal,XregG)
+MSEregG = metrics.mean_squared_error(Xoriginal, XregG)
 
 # Visualizziamo i risultati
 plt.figure(figsize=(30, 30))
 #Immagine originale
-ax1 = plt.subplot(1, 4, 1)
-ax1.imshow(X, cmap='gray', vmin=0, vmax=1)
+ax1 = plt.subplot(1, 2, 1)
+ax1.imshow(Xoriginal, cmap='gray', vmin=0, vmax=1)
 plt.title('Immagine Originale')
 
-#Nelle righe seguenti si gestiscono i vari livelli di corruzione dell'immagine, con i diversi blur e il noise applicato
-
-#Corruzzione 1: Kernel 5 x 5 e sigma uguale a 0.5
-ax2 = plt.subplot(1, 4, 2)
+#Immagine corrotta
+ax2 = plt.subplot(1, 2, 2)
 ax2.imshow(b, cmap='gray', vmin=0, vmax=1)
 plt.title(f'Immagine Corrotta (PSNR: {PSNR:.2f}) (MSE: {MSE:.6f})')
 
-#Corruzione 2: Kernel 7 x 7 e sigma uguale a 1
-ax3 = plt.subplot(1, 4, 3)
-ax3.imshow(b1, cmap='gray', vmin=0, vmax=1)
-plt.title(f'Immagine Corrotta (PSNR: {PSNR1:.2f}) (MSE: {MSE1:.6f})')
-
-#Corruzione 3: Kernel 9 x 9 e sigma uguale a 1.3
-ax4 = plt.subplot(1, 4, 4)
-ax4.imshow(b2, cmap='gray', vmin=0, vmax=1)
-plt.title(f'Immagine Corrotta (PSNR: {PSNR2:.2f}) (MSE: {MSE2:.6f})')
-
 plt.show()
 
 plt.figure(figsize=(30, 30))
-
-#Immagine minimizzata con soluzione naive K
+#Immagine minimizzata con soluzione naive 
 ax5 = plt.subplot(1,3,1)
-ax5.imshow(A(Xnaive,K),cmap='gray', vmin=0, vmax=1)
-plt.title(f'Immagine minimizzata con soluzione naive (PSNR: {PSNR3:.2f}) (MSE: {MSE3:.6f})')
+ax5.imshow(Xnaive,cmap='gray', vmin=0, vmax=1)
+plt.title(f'Immagine minimizzata con soluzione naive (PSNR: {PSNRnaive:.2f}) (MSE: {MSEnaive:.6f})')
 
-#Immagine minimizzata con soluzione naive K1
-ax6 = plt.subplot(1,3,2)
-ax6.imshow(A(Xnaive,K1),cmap='gray', vmin=0, vmax=1)
-plt.title(f'Immagine minimizzata con soluzione naive (PSNR: {PSNR31:.2f}) (MSE: {MSE31:.6f})')
+#Immagine minimizzata con regolarizzazione 
+ax8 = plt.subplot(1,3,2)
+ax8.imshow(XregCG,cmap='gray', vmin=0, vmax=1)
+plt.title(f'Immagine minimizzata con regolarizzazione (PSNR: {PSNRregCG:.2f}) (MSE: {MSEregCG:.6f})')
 
-#Immagine minimizzata con soluzione naive K2
-ax7 = plt.subplot(1,3,3)
-ax7.imshow(A(Xnaive,K2),cmap='gray', vmin=0, vmax=1)
-plt.title(f'Immagine minimizzata con soluzione naive (PSNR: {PSNR32:.2f}) (MSE: {MSE32:.6f})')
-
-plt.figure(figsize=(30, 30))
-
-#Immagine minimizzata con regolarizzazione K
-ax8 = plt.subplot(1,3,1)
-ax8.imshow(A(Xreg,K),cmap='gray', vmin=0, vmax=1)
-plt.title(f'Immagine minimizzata con regolarizzazione (PSNR: {PSNR4:.2f}) (MSE: {MSE4:.6f})')
-
-#Immagine minimizzata con regolarizzazione K1
-ax9 = plt.subplot(1,3,2)
-ax9.imshow(A(Xreg,K1),cmap='gray', vmin=0, vmax=1)
-plt.title(f'Immagine minimizzata con regolarizzazione (PSNR: {PSNR41:.2f}) (MSE: {MSE41:.6f})')
-
-#Immagine minimizzata con regolarizzazione K2
-ax10 = plt.subplot(1,3,3)
-ax10.imshow(A(Xreg,K2),cmap='gray', vmin=0, vmax=1)
-plt.title(f'Immagine minimizzata con regolarizzazione (PSNR: {PSNR42:.2f}) (MSE: {MSE42:.6f})')
+#Immagine minimizzata con regolarizzazione 2
+ax11 = plt.subplot(1,3,3)
+ax11.imshow(XregG,cmap='gray', vmin=0, vmax=1)
+plt.title(f'Immagine minimizzata con regolarizzazione 2 (PSNR: {PSNRregG:.2f}) (MSE: {MSEregG:.6f})')
 
 plt.show()
 
-plt.figure(figsize=(30, 30))
-
-#Immagine minimizzata con regolarizzazione 2
-ax11 = plt.subplot(1,3,1)
-ax11.imshow(A(Xreg2,K),cmap='gray', vmin=0, vmax=1)
-plt.title(f'Immagine minimizzata con regolarizzazione 2 (PSNR: {PSNR5:.2f}) (MSE: {MSE5:.6f})')
-
-#Immagine minimizzata con regolarizzazione 2
-ax12 = plt.subplot(1,3,2)
-ax12.imshow(A(Xreg2,K1),cmap='gray', vmin=0, vmax=1)
-plt.title(f'Immagine minimizzata con regolarizzazione 2 (PSNR: {PSNR51:.2f}) (MSE: {MSE51:.6f})')
-
-#Immagine minimizzata con regolarizzazione 2
-ax13 = plt.subplot(1,3,3)
-ax13.imshow(A(Xreg2,K2),cmap='gray', vmin=0, vmax=1)
-plt.title(f'Immagine minimizzata con regolarizzazione 2 (PSNR: {PSNR52:.2f}) (MSE: {MSE52:.6f})')
-
-plt.show()
+# print(f"PSNR: {PSNR,PSNRnaive,PSNRregCG,PSNRregG}")
+# print(f"MSE: {MSE,MSEnaive,MSEregCG,MSEregG}")
+# print(f"dev: {dev}")
